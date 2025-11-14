06229ff6330e9629c502a7f05e5de9d6cac48eed
Tomperez98 <tomasperezalvarez@gmail.com>
2025-11-14 16:26:34 -0500

    remove store related stuff

--- src/pycoro/kernel/t_aio/__init__.py
+++ src/pycoro/kernel/t_aio/__init__.py
@@ -6,16 +6,15 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from pycoro.kernel.t_aio.echo import EchoCompletion, EchoSubmission
     from pycoro.kernel.t_aio.function import FunctionCompletion, FunctionSubmission
-    from pycoro.kernel.t_aio.store import StoreCompletion, StoreSubmission
 
 
 @dataclass(frozen=True)
 class Submission:
     tags: dict[str, str]
-    value: EchoSubmission | StoreSubmission | FunctionSubmission
+    value: EchoSubmission | FunctionSubmission
 
 
 @dataclass(frozen=True)
 class Completion:
     tags: dict[str, str]
-    value: EchoCompletion | StoreCompletion | FunctionCompletion
+    value: EchoCompletion | FunctionCompletion
